50a4f54d148a6ce888e2a7c093b19de178a5ff90
Junya Ishihara <junya@champierre.com>
2014-10-03 11:16:49 +0900

    fix spec

--- spec/mega_lotto/drawing_spec.rb
+++ spec/mega_lotto/drawing_spec.rb
@@ -13,9 +13,15 @@ module MegaLotto
         expect(draw.size).to eq(5)
       end
 
+      it "each element is an integer" do
+        draw.each do |drawing|
+          expect(drawing).to be_a(Integer)
+        end
+      end
+
       it "each element is less than 60" do
         draw.each do |drawing|
-          expect(drawing).to be < 60
+          expect(drawing).to be <= 60
         end
       end
     end
